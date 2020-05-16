16861bb1060bf6ae8ef9587aa1387915ba1e7f21
wasd <pominov.denis@mail.ru>
2020-05-16 22:39:40 +0300

    tests updated

--- tests/tiny-test-git-fast-reword.sh
+++ tests/tiny-test-git-fast-reword.sh
@@ -2,6 +2,8 @@
 
 ((num_tests=10))
 
+mkdir tiny_test_bin
+cd tiny_test_bin || exit
 for ((i = 0; i < num_tests; i++))
 do
   touch test${i}.txt
@@ -13,5 +15,5 @@ git log --oneline
 
 for ((i = num_tests; i >= 0; i--))
 do
-  git reset --hard HEAD
+  git reset --hard HEAD~1
 done
