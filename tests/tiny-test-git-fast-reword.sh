a9e7ca4044f817923612a648e1821145c8dc3558
wasd <pominov.denis@mail.ru>
2020-05-16 22:52:34 +0300

    tests final version

--- tests/tiny-test-git-fast-reword.sh
+++ tests/tiny-test-git-fast-reword.sh
@@ -15,10 +15,14 @@ done
 
 git log --oneline
 
+cd ../
+
 for ((i = 0; i < num_tests; i++))
 do
   git-fast-reword HEAD~${i} "commit msg updated #${i}"
 done
+
+cd tiny_test_bin
 
 git log --oneline
 
